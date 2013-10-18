1de32a61a08dafe108772568f286f9c9297cace1
hia3 <mikhail.strelnikov@gmail.com>
2013-10-18 14:45:59 +0400

    fix vehicles dialog sizes for different UI sizes

--- resource/dialogs/RscVehicles.hpp
+++ resource/dialogs/RscVehicles.hpp
@@ -10,10 +10,11 @@ class RscVehicles {
 		class Mainback: RscText {
 			colorBackground[] = {0, 0, 0, 0.700000};
 			idc = -1;
-			x =-0.15;
-			y =-0.20;
-			w = 1.30;
-			h = 1.40;
+
+			x =-6 * GUI_GRID_W + GUI_GRID_X;
+			y =-5 * GUI_GRID_H + GUI_GRID_Y;
+			w = 52 * GUI_GRID_W;
+			h = 35 * GUI_GRID_H;
 		};
 	};
 	class controls {
@@ -21,10 +22,12 @@ class RscVehicles {
 		class FilterButtonCars : RscClickableText_ext {
 			idc = 3700;
 			style = 48 + 0x800;
-			x =-0.13;
-			y =-0.12;
-			w = 0.10;
-			h = 0.09;
+
+			x = -5.2 * GUI_GRID_W + GUI_GRID_X;
+			y = -3 * GUI_GRID_H + GUI_GRID_Y;
+			w = 4 * GUI_GRID_W;
+			h = 2.25 * GUI_GRID_H;
+
 			color[] = {1, 1, 1, 1};
 			colorActive[] = {0.7,1,0.7,1};
 			text = "\A3\ui_f\data\map\vehicleicons\iconCar_ca.paa";
@@ -32,62 +35,72 @@ class RscVehicles {
 		};
 		class FilterButtonArmor : FilterButtonCars {
 			idc = 3701;
-			x =-0.04;
+			x =-1.6 * GUI_GRID_W + GUI_GRID_X;
 			text = "\A3\ui_f\data\map\vehicleicons\iconTank_ca.paa";
 			action = "Dialog_VehicleFiller='armor'";
 		};
 		class FilterButtonHeli : FilterButtonCars {
 			idc = 3702;
-			x = 0.05;
+			x = 2 * GUI_GRID_W + GUI_GRID_X;
 			text = "\A3\ui_f\data\map\vehicleicons\iconHelicopter_ca.paa";
 			action = "Dialog_VehicleFiller='heli'";
 		};
 		class FilterButtonAir : FilterButtonCars {
 			idc = 3703;
-			x = 0.14;
+			x = 5.6 * GUI_GRID_W + GUI_GRID_X;
 			text = "\A3\ui_f\data\map\vehicleicons\iconPlane_ca.paa";
 			action = "Dialog_VehicleFiller='air'";
 		};
 		class FilterButtonShip : FilterButtonCars {
 			idc = 3711;
-			x = 0.23;
+			x = 9.2 * GUI_GRID_W + GUI_GRID_X;
 			text = "\A3\ui_f\data\map\vehicleicons\iconShip_ca.paa";
 			action = "Dialog_VehicleFiller='ship'";
 		};
 		class FilterButtonSupport : FilterButtonCars {
 			idc = 3704;
-			x = 0.32;
+
+			x = 12.8 * GUI_GRID_W + GUI_GRID_X;
+
 			text = "\A3\ui_f\data\map\vehicleicons\pictureRepair_ca.paa";
 			action = "Dialog_VehicleFiller='support'";
 		};
 
 		class BuyButton : RscShortcutButton {
 			idc = 3710;
-			x =-0.14;
-			y = 1.12;
-			w = 0.20;
-			h = 0.06;
+
+			x = -5.6 * GUI_GRID_W + GUI_GRID_X;
+			y = 28 * GUI_GRID_H + GUI_GRID_Y;
+			w = 8 * GUI_GRID_W;
+			h = 1.5 * GUI_GRID_H;
+
 			text = $STR_WF_Purchase;
 			action = "Dialog_VehicleActionSE='buy'";
 		};
 		class CloseButton : BuyButton {
 			idc = 3705;
-			x = 0.3;
+
+			x = 12 * GUI_GRID_W + GUI_GRID_X;
+
 			text = $STR_WF_Close;
 			action = "closeDialog 0";
 		};
 		class CA_Money_Value : RscText_ext {
 			idc = 3706;
-			x =-0.13;
-			y = 1.05;
+
+			x = -5.2 * GUI_GRID_W + GUI_GRID_X;
+			y = 26.25 * GUI_GRID_H + GUI_GRID_Y;
+
 			SizeEx = 0.04;
 			text = "";
 			colorText[] = {0.7,1,0.7,1};
 		};
 		class Gear_Title : CA_Money_Value {
 			idc = 3707;
-			x =-0.14;
-			y =-0.20;
+
+			x = -5.6 * GUI_GRID_W + GUI_GRID_X;
+			y = -5 * GUI_GRID_H + GUI_GRID_Y;
+
 			text = $STR_WF_Vehicles_Label;
 		};
 		class MainList : RscListBoxA_ext {
@@ -96,10 +109,11 @@ class RscVehicles {
 			drawSideArrows = 0;
 			idcRight = -1;
 			idcLeft = -1;
-			x =-0.15;
-			y = 0.00;
-			w = 0.65;
-			h = 1.00;
+
+			x = -6 * GUI_GRID_W + GUI_GRID_X;
+			y = 0 * GUI_GRID_H + GUI_GRID_Y;
+			w = 26 * GUI_GRID_W;
+			h = 25 * GUI_GRID_H;
 
 			sizeEx = 0.035;
 
@@ -107,10 +121,11 @@ class RscVehicles {
 		};
 		class SecondaryList: RscStructuredText_ext {
 			idc = 3709;
-			x = 0.50;
-			y = 0.00;
-			w = 0.65;
-			h = 1.00;
+
+			x = 20 * GUI_GRID_W + GUI_GRID_X;
+			y = 0 * GUI_GRID_H + GUI_GRID_Y;
+			w = 26 * GUI_GRID_W;
+			h = 25 * GUI_GRID_H;
 
 			size = 0.06;
 		};
